35547a2413dd0e47dd79f5d96af5a873c3455d3e
Sams <nicholettesams@msn.com>
2018-12-09 12:13:30 -0500

    Create a NavBar to store title and scores

---
+++ src/components/NavBar/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./style.css";
+
+const NavBar = props => (
+  <nav>
+    <ul>
+      <li className="brand">
+        <a href="/clicky-game/">{props.title}</a>
+      </li>
+
+      <li id="result">{props.result}</li>
+
+      <li id="current-score">Current Score: {props.score}</li>
+
+      <li id="top-score">Top Score: {props.topScore}</li>
+    </ul>
+  </nav>
+);
+
+export default NavBar;
