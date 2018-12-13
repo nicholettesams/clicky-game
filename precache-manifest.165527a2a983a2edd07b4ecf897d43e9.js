fae2212731d8999493b4c90ffc84b286ab18d665
Sams <nicholettesams@msn.com>
2018-12-13 15:12:40 -0700

    Updates

---
+++ precache-manifest.165527a2a983a2edd07b4ecf897d43e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "4a50d66b28581460142a",
+    "url": "/clicky-game/static/js/main.4a50d66b.chunk.js"
+  },
+  {
+    "revision": "3ad5f8a614c5e526e94e",
+    "url": "/clicky-game/static/js/1.3ad5f8a6.chunk.js"
+  },
+  {
+    "revision": "4a50d66b28581460142a",
+    "url": "/clicky-game/static/css/main.9394749b.chunk.css"
+  },
+  {
+    "revision": "3fbb7cad06ac426dd63bdcb908cc695c",
+    "url": "/clicky-game/index.html"
+  }
+];
